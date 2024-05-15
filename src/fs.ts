f425f939d00151d938c904a0efb91b294876892e
nnlgsakib <nlgarts@outlook.com>
2024-05-15 18:18:34 +0600

    updated Json schema

--- src/fs.ts
+++ src/fs.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 const dataPath = path.join(__dirname, '../_data');
 
-export function readData(): Promise<any[]> {
+export function readData(): Promise<{ [filename: string]: any }> {
     return new Promise((resolve, reject) => {
         fs.readdir(dataPath, (err, files) => {
             if (err) {
@@ -12,15 +12,16 @@ export function readData(): Promise<any[]> {
                 return;
             }
 
-            const jsonData: any[] = [];
+            const jsonData: { [filename: string]: any } = {};
 
             files.forEach(file => {
                 if (file.endsWith('.json')) {
+                    const fileName = file.replace('.json', '');
                     const filePath = path.join(dataPath, file);
                     const fileData = fs.readFileSync(filePath, 'utf8');
                     try {
                         const parsedData = JSON.parse(fileData);
-                        jsonData.push(parsedData);
+                        jsonData[fileName] = parsedData;
                     } catch (error) {
                         console.error(`Error parsing JSON file ${file}:`, error);
                     }
